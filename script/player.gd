272ac088af771f07fa44a07590ff5dd6e4bfdc2a
wawan-maulana <wawanme07@gmail.com>
2026-09-03 10:27:14 +0700

    inputmap

--- script/player.gd
+++ script/player.gd
@@ -14,11 +14,11 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * GRAVITY_MULTIPLIER * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("lompat") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get input for up/down, and force rightward movement for x
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("kiri", "kanan", "ata", "bawah")
 	input_dir.x = max(1.0, input_dir.x) # Automatically forces the character to move right (or faster if right is pressed)
 	
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
